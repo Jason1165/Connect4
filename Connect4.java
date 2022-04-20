dda2270ff29420ba84a62d588ca00b4030419234
Jason Lin <jlin31@stuy.edu>
2022-04-19 20:50:24 -0400

    Insert Chip Logic

--- Connect4.java
+++ Connect4.java
@@ -1,22 +1,24 @@
 import java.util.*;
 public class Connect4 {
-  private int[][] board;
+  private char[][] board;
   private boolean yourTurn;
   private boolean bot;
   private ArrayList<Integer> moves;
 
   public Connect4() {
-    board = new int[6][7];
+    board = new char[6][7];
     moves = new ArrayList<Integer>();
     yourTurn = true;
     bot = false;
+    fill();
   }
 
   public Connect4(int height, int width) {
-    board = new int[height][width];
+    board = new char[height][width];
     moves = new ArrayList<Integer>();
     yourTurn = true;
     bot = false;
+    fill();
   }
 
   private void toggleTurn() {
@@ -25,6 +27,14 @@ public class Connect4 {
 
   private void toggleBot() {
     bot = !bot;
+  }
+
+  private void fill() {
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[0].length; j++) {
+        board[i][j] = '0';
+      }
+    }
   }
 
   public String toString() {
@@ -37,4 +47,28 @@ public class Connect4 {
     }
     return str;
   }
+
+  public boolean insertChip(int column) {
+    if (column >= board[0].length || column < 0) {
+      throw new IllegalArgumentException("Column " + column + " is not a valid column");
+    }
+    int position = -1;
+    for (int i = 0; i < board.length; i++) {
+      if (board[i][column] == '0') {
+        position = i;
+      }
+    }
+    if (position >= 0) {
+      if (yourTurn) {
+        board[position][column] = 'R';
+      }
+      else {
+        board[position][column] = 'Y';
+      }
+      toggleTurn();
+      return true;
+    }
+    return false;
+  }
+
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,11 @@
 public class Tester {
   public static void main(String[] args) {
-    
+    Connect4 boardA = new Connect4();
+    System.out.println(boardA);
+    boardA.insertChip(0);
+    boardA.insertChip(0);
+    boardA.insertChip(0);
+    boardA.insertChip(0);
+    System.out.println(boardA);
   }
 }
